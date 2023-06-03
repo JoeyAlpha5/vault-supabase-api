feb0840b4858170e1032649146717b3c5aff2b2e
Jalome <jalome@click2sure.co.za>
2023-06-04 01:06:45 +0200

    initial commit

---
+++ supabase/functions/vault-get-products/index.ts
@@ -0,0 +1,37 @@
+// Follow this setup guide to integrate the Deno language server with your editor:
+// https://deno.land/manual/getting_started/setup_your_environment
+// This enables autocomplete, go to definition, etc.
+
+import { serve } from "deno"
+import Stripe from "stripe";
+
+const stripe: any = new Stripe(Deno.env.get('STRIPE_API_KEY') as string,{
+  // This is needed to use the Fetch API rather than relying on the Node http
+  // package.
+  apiVersion: '2022-11-15',
+  httpClient: Stripe.createFetchHttpClient(),
+});
+
+serve(async (req) => {
+  // get products from stripe
+  try{
+    const products = await stripe.products.list();
+    return new Response(
+      JSON.stringify(products.data),
+      { headers: { "Content-Type": "application/json" } },
+    )
+  }
+  catch(err){
+    return new Response(
+      JSON.stringify(err),
+      { headers: { "Content-Type": "application/json" } },
+    )
+  }
+
+})
+
+// To invoke:
+// curl -i --location --request POST 'http://localhost:54321/functions/v1/' \
+//   --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
+//   --header 'Content-Type: application/json' \
+//   --data '{"name":"Functions"}'
